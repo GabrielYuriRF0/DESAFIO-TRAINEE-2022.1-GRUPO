e305b13f84dbc394d6ca14c4437ab310af00978a
GabrielYuriRF0 <gabrie.yuri1020@gmail.com>
2022-11-18 21:11:22 -0300

    Criação do model da entidade tarefa

---
+++ API - PsCodeX/model/Tarefa.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const bcrypt = require('bcrypt');
+
+const TarefaSchema = new Schema({
+    nome: {type: String, required:true},
+    descricao: {type:String, lowercase:true},
+    data: {type:Date, default:Date.now},
+    concluida:{type:Boolean, default:false},
+    usuario:{type: mongoose.ObjectId, ref: 'Login', required:true}
+
+});
+
+module.exports = mongoose.model('Tarefa', this.schema, 'tarefas');
